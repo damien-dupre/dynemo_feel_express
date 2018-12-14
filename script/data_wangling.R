82ea5beb01abff0c8ba5e0105576f293cffd2981
damien-dupre <damien.dupre.ac@gmail.com>
2018-12-14 17:14:52 +0000

    init

---
+++ script/data_wangling.R
@@ -0,0 +1,5 @@
+# setup -------------------------------------------------------------------
+library(here)
+library(tidyverse)
+# upload data -------------------------------------------------------------
+data_sr <- read_rds(here::here("data/Base_Finale_20090114.rds"))
